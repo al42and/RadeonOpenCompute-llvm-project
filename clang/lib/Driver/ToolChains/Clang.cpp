65ec9230124e0d4c643d5abae557eb0d748bf3dc
Scott Linder <Scott.Linder@amd.com>
2022-06-07 17:59:48 +0000

    [HeterogeneousDWARF][Clang] Add -gheterogeneous-dwarf
    Add a frontend option to request the heterogeneous DWARF extensions.
    Make this the default for AMDGCN.
    
    Change-Id: I5be91c8dc3b8b07694d3b7ead040912e640d1e9d

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -4409,6 +4409,9 @@ static void renderDebugOptions(const ToolChain &TC, const Driver &D,
   bool EmitDwarfForAMDGCN = EmitDwarf && T.isAMDGCN();
   if (EmitDwarfForAMDGCN)
     CmdArgs.append({"-mllvm", "-amdgpu-spill-cfi-saved-regs"});
+  if (Args.hasFlag(options::OPT_gheterogeneous_dwarf,
+                   options::OPT_gno_heterogeneous_dwarf, EmitDwarfForAMDGCN))
+    CmdArgs.push_back("-gheterogeneous-dwarf");
 }
 
 void Clang::ConstructJob(Compilation &C, const JobAction &JA,
